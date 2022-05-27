cc8042096ab936f042b371fa84f9d3e656f6cf49
metachoSamuel <samuel.f.met@gmail.com>
2022-05-27 03:01:41 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Aclonica&display=swap');
+        @import url('https://fonts.googleapis.com/css2?family=Lora&display=swap');
+    </style>
+
+    <script src="//code.jivosite.com/widget/bsfhZj86wJ" async></script>
+    
+    <title>True</title>
+</head>
+
+<header>
+    <div class="logo">
+       
+        <img src="" alt="">
+    </div>
+    <div class="login">
+        <form action="">
+            <input type="submit" value="Iniciar sesion">
+        </form>
+        <form action="">
+            <input type="submit" value="Registrarse">
+        </form>
+    </div>
+    <nav>
+        <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Mision y Vision</a></li>
+            <li><a href="#">Estadisticas</a></li>
+        </ul>
+    </nav>
+</header>
+
+<body>    
+<div class="banner">
+        
+        <img src="" alt="">
+    </div>
+    <div name="" class="true">
+        <h3>¿Que es True?</h3>
+        <p>True es un videojuego de aventura donde el principal objetivo es descubrir y aprender sobre la verdadera historia de Colombia, aquí no encontraras héroes o villanos, acá encontraras la verdad.<br>
+            Aunque no siempre tendrás un final feliz, podrás viajar al pasado y entender el porqué de nuestra historia, el porqué de nuestra independencia y el porqué de nuestro presente.<br>
+        </p>
+    </div>
+    <div name="" class="mision">
+        <h3>Mision</h3>
+        <p>True es un juego enfocado en contarnos las historias del pasado que fueron olvidando se con el transcurso del tiempo, dirigido para todo público pero el énfasis de aprendizaje es en los jóvenes, dejando aun lado el desconocimiento para no repetir las mismas historias, te contara lo que tu escuela y tu gobierno quiere que ignores.</p>
+    </div>
+    <div name="" class="vision">
+        <h3>Vision</h3>
+        <p>Se creará un juego basado en hechos reales donde cada misión será un fragmento de la historia de nuestro país que será distribuido por todas las gamestore que puedas encontrar online, proyectado su lanzamiento en el año 2023 de forma gratuita para todos.</p>
+    </div>
+    <footer>
+        <div class="redes">
+            
+            <a href="#"><img src="" alt=""></a>
+            <a href="#"><img src="" alt=""></a>
+            <a href=""><img src="" alt=""></a>
+        </div>
+        <div class="logo_footer">
+            
+            <img src="" alt="">
+        </div>        
+    </footer>
+</body>
+</html>
