094609b508876fce08f8e87d1bbe091069928062
metachoSamuel <samuel.f.met@gmail.com>
2022-06-02 14:05:47 -0500

    x

--- partials/userdetail.php
+++ partials/userdetail.php
@@ -1,11 +1,29 @@
 <?php session_start(); ?>
-
-<h2> Bienvenido. <?= $user['email']; ?></h2>
+<script src="librerias/plotly-2.12.1.min.js" ></script>
+<h2> Bienvenido. <?= $user['username']; ?></h2>
 <br>Ingresaste satisfactoriamente
 
+<div id="graficaLineal"></div>
+
+    <script type="text/javascript">
+        var trace1 = {
+            x: [1, 2, 3, 4],
+            y: [10, 15, 30, 17],
+            type: 'scatter'
+        }
+
+        var trace2 = {
+            x: [1, 2, 3, 4],
+            y: [16, 5, 11, 9],
+            type: 'scatter'
+        }
+
+        var data = [trace1, trace2];
+
+        Plotly.newPlot('graficaLineal', data);
+    </script>
 
 <div id="cargaLineal"></div>
-
 
 <script type="text/javascript">
     $(document).ready(function(){
